543f647bede2f084cf49cd287e73fe816b2d4173
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2023-03-21 11:54:28 +0200

    21 mar 2023 11:54 am.

--- lib/helpers/consts.dart
+++ lib/helpers/consts.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color mainColor = const Color(0xff5985CB);
+
+Duration animationDuration = const Duration(milliseconds: 200);
--- lib/screens/main_screens/tabs_screen.dart
+++ lib/screens/main_screens/tabs_screen.dart
@@ -1,3 +1,8 @@
+import 'package:acacus/helpers/consts.dart';
+import 'package:acacus/screens/main_screens/discover_screen.dart';
+import 'package:acacus/screens/main_screens/home_screen.dart';
+import 'package:acacus/screens/main_screens/notification_screen.dart';
+import 'package:acacus/screens/main_screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -13,41 +18,67 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: const Center(
-        child: Text("TABS"),
+      body: AnimatedSwitcher(
+        duration: animationDuration,
+        child: currentIndex == 0
+            ? const ProfileScreen()
+            : currentIndex == 1
+                ? const NotificationScreen()
+                : currentIndex == 2
+                    ? const DiscoverScreen()
+                    : const HomeScreen(),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         elevation: 0,
         currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.red,
         items: [
           BottomNavigationBarItem(
               label: '',
               icon: Image.asset(
                 'assets/icons/profileIcon.png',
-                width: size.width / 10,
-                height: size.width / 10,
+                width: size.width / 13,
+                height: size.width / 13,
+                fit: BoxFit.contain,
+                color:
+                    currentIndex == 0 ? mainColor : mainColor.withOpacity(0.3),
               )),
           BottomNavigationBarItem(
               label: '',
               icon: Image.asset(
                 'assets/icons/notificationIcon.png',
-                width: size.width / 10,
-                height: size.width / 10,
+                width: size.width / 13,
+                height: size.width / 13,
+                fit: BoxFit.contain,
+                color:
+                    currentIndex == 1 ? mainColor : mainColor.withOpacity(0.3),
               )),
           BottomNavigationBarItem(
               label: '',
               icon: Image.asset(
                 'assets/icons/discoverIcon.png',
-                width: size.width / 10,
-                height: size.width / 10,
+                width: size.width / 13,
+                height: size.width / 13,
+                fit: BoxFit.contain,
+                color:
+                    currentIndex == 2 ? mainColor : mainColor.withOpacity(0.3),
               )),
           BottomNavigationBarItem(
               label: '',
               icon: Image.asset(
                 'assets/icons/homeIcon.png',
-                width: size.width / 10,
-                height: size.width / 10,
+                width: size.width / 13,
+                height: size.width / 13,
+                color:
+                    currentIndex == 3 ? mainColor : mainColor.withOpacity(0.3),
+                fit: BoxFit.contain,
               ))
         ],
       ),
